76143f0a9080b125bd101135a293d224c57f3790
Olivier Tilmans <olivier.tilmans@uclouvain.be>
2015-04-10 10:48:27 +0200

    Append NAT rules on top of existing one if any
    Signed-off-by: Olivier Tilmans <olivier.tilmans@uclouvain.be>

--- scripts/post_up.sh
+++ scripts/post_up.sh
@@ -2,7 +2,7 @@
 # Setting up NAT
 
 echo "Will setup masquerading. Sudo might ask your password..."
-sudo iptables -t nat -A POSTROUTING -s 192.168.33.0/24 -j MASQUERADE
+sudo iptables -t nat -I POSTROUTING -s 192.168.33.0/24 -j MASQUERADE
 
 for f in user_scripts/up/* ; do 
 	if [[ -x $f ]] ; then
