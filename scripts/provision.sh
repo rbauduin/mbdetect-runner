7628cf7f9f9c171d1fee06a2aa717961e7f6cf3a
Bauduin Raphaël <raphael.bauduin@uclouvain.be>
2015-04-10 10:49:39 +0200

    set prefered source in default route, and masquerade 10.0.0.0/8 traffic

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -4,5 +4,9 @@ set -x
 # set default route via the bridge, not the nat
 host_ipv4="192.168.33.1"
 
+# set default route with prefered source address
 ip route del default via 10.0.2.2
-ip route add default via $host_ipv4
+ip route add default via $host_ipv4 src 192.168.33.10
+
+# NAT of packets sent with source 10.0.0.0/8
+iptables -t nat -A POSTROUTING -o eth1 -s 10.0.0.0/8 -j MASQUERADE
