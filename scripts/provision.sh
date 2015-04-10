b33c254d9d385a07bfba9c762a5ecff9cff30c93
Bauduin Raphaël <raphael.bauduin@uclouvain.be>
2015-04-10 09:51:42 +0200

    added nat for 10.0.0.0/8 range in vm itself

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -6,3 +6,4 @@ host_ipv4="192.168.33.1"
 
 ip route del default via 10.0.2.2
 ip route add default via $host_ipv4 src 192.168.33.10
+iptables -t nat -A POSTROUTING -o eth1 -s 10.0.0.0/8 -j MASQUERADE
